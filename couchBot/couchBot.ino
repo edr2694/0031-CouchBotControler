288e1789f6da123e1de9ea56f41710863d0df419
edr2694 <edr2694@g.rit.edu>
2023-07-04 23:59:14 -0400

    starting to build out CLI

--- couchBot/couchBot.ino
+++ couchBot/couchBot.ino
@@ -1,22 +1,25 @@
 #include <Servo.h>
+#include <EEPROM.h> // for saving values
 
 
-const uint8_t LEFT_SERVO_PIN = 5;
-const uint8_t RIGHT_SERVO_PIN = 6;
-const uint8_t RC_CH1 = A0;
-const uint8_t RC_CH2 = A1;
-const uint8_t RC_CH3 = A2;
-const uint8_t RC_CH8 = A3; // the knob, to be used for either turn specific sensitivity or absolute maximum lockout
-const uint16_t CH1_MAX = 1924; // right stick right/left TURN
-const uint16_t CH1_MIN = 963;
-const uint16_t CH2_MAX = 1989; // right stick up/down MAIN THROTTLE
-const uint16_t CH2_MIN = 984;
-const uint16_t CH3_MAX = 1800; // left stick up/down LIMITTER
-const uint16_t CH3_MIN = 1100;
-const uint16_t CH8_MIN = 989; // secondary limiter
-const uint16_t CH8_MAX = 1984;
-const uint8_t reverseRight = 1;
-const uint8_t reverseLeft = 0;
+const uint8_t  LEFT_SERVO_PIN  = 5;
+const uint8_t  RIGHT_SERVO_PIN = 6;
+const uint8_t  RC_CH1          = A0;
+const uint8_t  RC_CH2          = A1;
+const uint8_t  RC_CH3          = A2;
+const uint8_t  RC_CH8          = A3; // the knob, to be used for either turn specific sensitivity or absolute maximum lockout
+const uint16_t CH1_MAX         = 1924; // right stick right/left TURN
+const uint16_t CH1_MIN         = 963;
+const uint16_t CH2_MAX         = 1989; // right stick up/down MAIN THROTTLE
+const uint16_t CH2_MIN         = 984;
+const uint16_t CH3_MAX         = 1800; // left stick up/down LIMITTER
+const uint16_t CH3_MIN         = 1100;
+const uint16_t CH8_MIN         = 989; // secondary limiter
+const uint16_t CH8_MAX         = 1984;
+const uint16_t veloDeadZone    = 6;
+const uint16_t diffDeadZone    = 6;
+const uint8_t  reverseRight    = 1;
+const uint8_t  reverseLeft     = 0;
 
 
 //working variables
@@ -28,13 +31,22 @@ uint32_t timeout = 100;
 uint32_t lastTime = 0;
 int16_t velocity = 0;
 int16_t difference = 0;
+uint16_t maxDifference = 20;
 int16_t maxSpeed = 0;
 int16_t leftMotorSpeed = 0;
 int16_t rightMotorSpeed = 0;
+uint8_t maxPWM = 180;
+uint8_t minPWM = 0;
 uint8_t maxLeftPWM = 180;
 uint8_t minLeftPWM = 0;
 uint8_t maxRightPWM = 180;
 uint8_t minRightPWM = 0;
+boolean deadZoneEnable = true;
+boolean outputEnable = false;
+boolean serialDebugPrintEnable = false;
+boolean newCommand = false;
+boolean elevatedPermissions = false;
+String commandStr = "";
 
 
 Servo leftMotor;
@@ -49,10 +61,21 @@ void setup() {
   pinMode(RC_CH2, INPUT);
   pinMode(RC_CH3, INPUT);
   pinMode(RC_CH8, INPUT);
+  if (reverseLeft) {
+    maxLeftPWM = 0;
+    minLeftPWM = 180;
+  }
+  if (reverseRight) {
+    maxRightPWM = 0;
+    minRightPWM = 180;
+  }
 }
 
 
 void loop() {
+  if (newCommand) {
+    handleCommand(); // do this first, because if anything changed we want to implement it immediately.
+  }
   int32_t ch1Average = 0;
   int32_t ch2Average = 0;
   int32_t ch3Average = 0;
@@ -70,7 +93,9 @@ void loop() {
   CH8_last_value = ch8Average / 3.0;
   scaleNumbers();
   controlMotor();
-  serialUpdate();
+  if (serialDebugPrintEnable) {
+    serialDebugPrint();
+  }
 }
 
 
@@ -100,11 +125,7 @@ void getReceiver() {
 
 
 void scaleNumbers() {
-// set current turn value
-  if (CH1_last_value > CH1_MAX) difference = CH1_MAX;
-  else if (CH1_last_value < CH1_MIN) difference = CH1_MIN;
-  else difference = CH1_last_value;
-  difference = map(difference, CH1_MIN, CH1_MAX, -99, 99);
+
 
 // set current speed value
   if (CH2_last_value > CH2_MAX) velocity = CH2_MAX;
@@ -118,28 +139,26 @@ void scaleNumbers() {
   else maxSpeed = CH3_last_value;
   maxSpeed = map(maxSpeed, CH3_MIN, CH3_MAX, 1, 99);
 
+// set current turn value
+  if (CH1_last_value > CH1_MAX) difference = CH1_MAX;
+  else if (CH1_last_value < CH1_MIN) difference = CH1_MIN;
+  else difference = CH1_last_value;
+  difference = map(difference, CH1_MIN, CH1_MAX, (-1*maxDifference), maxDifference);
+
   // set everything to 0 if transmitter is off
   if (CH1_last_value == 0) difference = 0;  //CH1 returns 0 if radio is off
-  if (CH2_last_value == 0) velocity = 0;  //CH2 returns 0 if radio is off
-  if (CH3_last_value == 0) velocity = 0;  //CH3 returns 0 if radio is off
-  if (CH8_last_value == 0) velocity = 0;  //CH8 returns 0 if radio is off
+  if (CH2_last_value == 0) velocity = 0;    //CH2 returns 0 if radio is off
+  if (CH3_last_value == 0) velocity = 0;    //CH3 returns 0 if radio is off
+  if (CH8_last_value == 0) velocity = 0;    //CH8 returns 0 if radio is off
 
 }
 
 
 void controlMotor() {
-  if (reverseLeft) {
-    maxLeftPWM = 0;
-    minLeftPWM = 180;
+  if (deadZoneEnable) {
+    if (abs(velocity) <= veloDeadZone) velocity = 0;
+    if (abs(difference) <= diffDeadZone) difference = 0;
   }
-  if (reverseRight) {
-    maxRightPWM = 0;
-    minRightPWM = 180;
-  }
-
-  if (velocity >= -6 && velocity <= 6) velocity = 0;
-  if (difference >= -6 && difference <= 6) difference = 0;
-
   leftMotorSpeed = constrain((maxSpeed/99.0)*(velocity + difference),-99,99);
   rightMotorSpeed = constrain((maxSpeed/99.0)*(velocity - difference),-99,99);
 
@@ -147,8 +166,27 @@ void controlMotor() {
   rightMotor.write(map(rightMotorSpeed, -99, 99, minRightPWM, maxRightPWM));
 }
 
+void handleCommand() {
+    if (commandStr.length() < 1) {
+        invalidCommand();
+    } else if (commandStr.equalsIgnoreCase("enable")) {
+        outputEnable = true;
+    } else if (commandStr.equalsIgnoreCase("disable")) {
+        outputEnable = false;
+    } else if (commandStr.equalsIgnoreCase("help")) {
+        printHelp();
+    } else if (commandStr.equalsIgnoreCase("lpv")) {
+        printParameters();
+    } else {
 
-void serialUpdate() {
+    }
+}
+
+void invalidCommand() {
+    Serial.println("Invalid Command!");
+}
+
+void serialDebugPrint() {
   Serial.print("CH1: "); Serial.print(CH1_last_value);
   Serial.print("  CH2: "); Serial.print(CH2_last_value);
   Serial.print("  CH3: "); Serial.print(CH3_last_value);
@@ -160,3 +198,51 @@ void serialUpdate() {
   Serial.print("  R: "); Serial.println(rightMotorSpeed);
 }
 
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    commandStr += inChar;
+    if (inChar == '\n') {
+      newCommand = true;
+    }
+  }
+}
+
+void printHelp() {
+    Serial.println("******Caddy Couch Control V0.2******");
+    Serial.println("Available Commands:");
+    Serial.println("help - print this message");
+    Serial.println("lpv - list all parameter values");
+    Serial.println("dbg - print debug values (q to quit)");
+    Serial.println("enable - enable motor output");
+    Serial.println("disable - disable motor output");
+    if (elevatedPermissions) {
+        Serial.println("You have elevated permissions! You have extra access:");
+        Serial.println("lcp - list changeable parameters");
+        Serial.println("cp <parameter> <value> - change a parameter value. Motors must be disabled");
+        Serial.println("save - save parameters to eeprom. Will overwrite any values stored there, and cannot be undone");
+        Serial.println("reload - reload parameters from eeprom");
+    }
+}
+
+void printParameters() {
+    Serial.println("***Current Parameters***");
+    Serial.print("outputEnable");Serial.println(outputEnable);
+    Serial.print("maxDifference");Serial.println(maxDifference);
+    Serial.print("maxPWM");Serial.println(maxPWM);
+    Serial.print("minPWM");Serial.println(minPWM);
+    Serial.print("veloDeadZone");Serial.println(veloDeadZone);
+    Serial.print("diffDeadZone");Serial.println(diffDeadZone);
+    Serial.print("deadZoneEnable");Serial.println(deadZoneEnable);
+    Serial.print("reverseRight");Serial.println(reverseRight);
+    Serial.print("reverseLeft");Serial.println(reverseLeft);
+    Serial.print("elevatedPermissions");Serial.println(elevatedPermissions);
+    Serial.print("ch1max");Serial.println(CH1_MAX);
+    Serial.print("ch1min");Serial.println(CH1_MIN);
+    Serial.print("ch2max");Serial.println(CH2_MAX);
+    Serial.print("ch2min");Serial.println(CH2_MIN);
+    Serial.print("ch3max");Serial.println(CH3_MAX);
+    Serial.print("ch3min");Serial.println(CH3_MIN);
+    Serial.print("ch8max");Serial.println(CH8_MAX);
+    Serial.print("ch8min");Serial.println(CH8_MIN);
+}
